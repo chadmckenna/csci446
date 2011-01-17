e2cc37ad29bed8a4534ca9eff43111b6176b0e57
Chad McKenna <cmckenna@mines.edu>
2011-01-16 20:49:15 -0700

    Adding CSS and JAVASCRIPT directories

---
+++ project01/javascript/master.js
@@ -0,0 +1,15 @@
+
+function show_tooltip(id) {
+	
+}
+
+function hide_tooltip() {
+	
+}
+
+function check() {
+	ele = document.getElementById('feeling');
+	if (ele.value == "") {
+		alert("Surely you feel something!");
+	}
+}
