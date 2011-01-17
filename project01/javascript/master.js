692583c202215bf35c26898e112017782d88b6ec
Chad McKenna <cmckenna@mines.edu>
2011-01-17 00:51:26 -0700

    Add hover command to NAV bar in index.html

--- project01/javascript/master.js
+++ project01/javascript/master.js
@@ -1,10 +1,74 @@
+/* Large portion of this script was adapted from 
+ * my summer intern project with InfoPrint Solutions
+ * Copyright 2006, 2007, 2011:
+ * Bontrager Connection, LLC and Chad McKenna
+ */
 
-function show_tooltip(id) {
-	
+var cX = 0;
+var cY = 0;
+var rX = 0;
+var rY = 0;
+function UpdateCursorPosition(e) {
+	cX = e.pageX;
+	cY = e.pageY;
 }
 
-function hide_tooltip() {
-	
+function UpdateCursorPositionDocAll(e) { 
+	cX = event.clientX; 
+	cY = event.clientY;
+}
+
+	if(document.all) {
+		document.onmousemove = UpdateCursorPositionDocAll; 
+	} else {
+		document.onmousemove = UpdateCursorPosition;
+	}
+
+function AssignPosition(d) {
+	if(self.pageYOffset) {
+		rX = self.pageXOffset;
+		rY = self.pageYOffset;
+	} else if (document.documentElement && document.documentElement.scrollTop) {
+		rX = document.documentElement.scrollLeft;
+		rY = document.documentElement.scrollTop;
+	} else if (document.body) {
+		rX = document.body.scrollLeft;
+		rY = document.body.scrollTop;
+	}
+
+	if(document.all) {
+		cX += rX; 
+		cY += rY;
+	}
+	d.style.left = (cX-200) + "px";
+	d.style.top = (cY+15) + "px";
+}
+function HideContent(d) {
+	if(d.length < 1) {
+		return;
+	}
+		
+	document.getElementById(d).style.display = "none";
+}
+function ShowContent(d) {
+	if(d.length < 1) { 
+		return;
+	}
+	var dd = document.getElementById(d);
+	AssignPosition(dd);
+	dd.style.display = "block";
+}
+function ReverseContentDisplay(d) {
+	if(d.length < 1) {
+		return;
+	}
+	var dd = document.getElementById(d);
+	AssignPosition(dd);
+	if(dd.style.display == "none") {
+		dd.style.display = "block";
+	} else {
+		dd.style.display = "none";
+	}
 }
 
 function check() {
