e7a1c5c84764ea429ab5357b5b4a2d038a2aa177
Chad McKenna <cmckenna@mines.edu>
2011-02-27 22:53:12 -0700

    More styling complete. Also, finished authlogic base of application.

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
-  map.login "logout", :controller => "user_sessions", :action => "destroy"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
   
   map.resources :user_sessions
   map.resources :users
