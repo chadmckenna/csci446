4da1dbdacb65da4b55ad08a3d157deb9976cd053
Chad McKenna <cmckenna@mines.edu>
2011-03-06 17:20:22 -0700

    Almost done with declaritive authorization.

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,10 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :roles
+
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
   
   map.resources :user_sessions
   map.resources :users
   map.resources :games
+  map.resources :roles
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
